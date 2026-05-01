040c839e919bc683a365da0b2dc3156531d0852e
Ganbaa0 <ganbaatar.ganbaa1128@gmail.com>
2026-05-01 10:42:45 +0800

    first commit

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse
 from store.models import Product
  
 # Create your views here.
+def home(request):
+    Products = Product.objects.all()
+    return render(request, 'home.html', {'Products': Products})
 def cart(request):
     cart = Cart(request)
     cart_products = cart.get_prods()
